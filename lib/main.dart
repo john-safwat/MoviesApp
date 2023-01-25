9277f17b5468fd25a2ea584bb7349b9727e8e4a2
john safwat <john362sss@gmail.com>
2023-01-25 10:30:29 +0200

    add the first files home screen theme class main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:movies/Presentation/Home/HomeScreen.dart';
+import 'package:movies/Presentation/UI/Home/HomeScreen.dart';
 
 void main () {
   runApp(MyApp());
